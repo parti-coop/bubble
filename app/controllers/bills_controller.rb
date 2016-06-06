1f7ec89a24fe5224c5bbe00ff6532e42ffbffa18
dali <dalikim@parti.xyz>
2016-06-07 00:20:33 +0900

    bugfix voting

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -3,11 +3,12 @@ class BillsController < ApplicationController
 
   def upvote
     @bill = Bill.find_by slug: params[:slug]
-
+    @upvoted = false
     return if upvoted?(@bill)
     if @bill.present?
       @bill.upvotes_count += 1
       if @bill.save
+        @upvoted = true
         mark_upvoted(@bill)
       end
     end
