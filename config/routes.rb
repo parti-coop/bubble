72ab1450326f43038fddc6a4e86baff7cf99c063
qus <qus1225@gmail.com>
2016-06-30 16:07:07 +0900

    법안과정 모바일용 이미지연결, 푸터로고이미지 교체

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'step1', to: 'pages#step1'
+  get 'step2', to: 'pages#home'
 
   post 'bills/:slug/upvote', to: 'bills#upvote', as: :upvote_bill
   post 'party_names/upvote', to: 'party_names#upvote', as: :upvote_party_name
