a414798f1e8cf2351b8a7845101e2bc727c67d25
dali <dalikim@parti.xyz>
2016-07-14 23:09:51 +0900

    추천수 캐쉬 및 네비게이션

--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -1,3 +1,4 @@
 class Upvote < ActiveRecord::Base
   belongs_to :user
+  belongs_to :post, counter_cache: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160714115330) do
+ActiveRecord::Schema.define(version: 20160714135432) do
 
   create_table "bills", force: :cascade do |t|
     t.string   "slug",          limit: 255,             null: false
@@ -45,12 +45,13 @@ ActiveRecord::Schema.define(version: 20160714115330) do
     t.integer  "user_id",       limit: 4
     t.string   "guest_name",    limit: 255
     t.string   "guest_email",   limit: 255
-    t.string   "title",         limit: 255,   null: false
+    t.string   "title",         limit: 255,               null: false
     t.text     "body",          limit: 65535
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.string   "board_slug",    limit: 255,   null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
+    t.string   "board_slug",    limit: 255,               null: false
     t.integer  "sequential_id", limit: 4
+    t.integer  "upvotes_count", limit: 4,     default: 0
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
