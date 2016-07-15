3256d74512d22cec8114f446c508acb112ab8e83
dali <dalikim@parti.xyz>
2016-07-15 18:36:50 +0900

    퀴즈 결과 소셜공유 버튼

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -86,6 +86,54 @@ var prepare_posts_list = function($base) {
   });
 }
 
+var prepare_social_share = function($base) {
+  $base.find('[data-action="bubble-share"]').each(function(i, elm) {
+    var $elm = $(elm);
+
+    var url = $elm.data('share-url');
+    var text = $elm.data('share-text');
+    var share = $elm.data('share-provider');
+    if ($.is_blank(share)) return;
+    var image_url = $elm.data('share-image');
+    var image_width = $elm.data('share-image-width');
+    var image_height = $elm.data('share-image-height');
+
+    switch(share) {
+    case 'kakao-link':
+      Kakao.Link.createTalkLinkButton({
+        container: elm,
+        label: text,
+        image: {
+          src: image_url,
+          width: image_width,
+          height: image_height
+        },
+        webLink: {
+          text: '바글시민 와글입법에서 보기',
+          url: url
+        }
+      });
+    break
+    case 'kakao-story':
+      $elm.on('click', function(e) {
+        Kakao.Story.share({
+          url: url,
+          text: text
+        });
+      });
+    break
+    default:
+      $elm.jsSocials({
+        showCount: false,
+        showLabel: false,
+        shares: [share],
+        text: text,
+        url: url
+      });
+    }
+  });
+}
+
 $(function(){
 
   $('map').imageMapResize();
@@ -140,58 +188,13 @@ $(function(){
     }
   });
 
-  $('[data-action="bubble-share"]').each(function(i, elm) {
-    var $elm = $(elm);
-
-    var url = $elm.data('share-url');
-    var text = $elm.data('share-text');
-    var share = $elm.data('share-provider');
-    if ($.is_blank(share)) return;
-    var image_url = $elm.data('share-image');
-    var image_width = $elm.data('share-image-width');
-    var image_height = $elm.data('share-image-height');
-
-    switch(share) {
-    case 'kakao-link':
-      Kakao.Link.createTalkLinkButton({
-        container: elm,
-        label: text,
-        image: {
-          src: image_url,
-          width: image_width,
-          height: image_height
-        },
-        webLink: {
-          text: '바글시민 와글입법에서 보기',
-          url: url
-        }
-      });
-    break
-    case 'kakao-story':
-      $elm.on('click', function(e) {
-        Kakao.Story.share({
-          url: url,
-          text: text
-        });
-      });
-    break
-    default:
-      $elm.jsSocials({
-        showCount: false,
-        showLabel: false,
-        shares: [share],
-        text: text,
-        url: url
-      });
-    }
-  });
-
   $('[data-action="bubble-naming-option"]').change(function() {
     $($(this).data('sibling')).removeClass("selected")
     $(this).closest("label").toggleClass("selected", this.checked);
   });
 
-  prepare_posts_list($('html'))
+  prepare_posts_list($('html'));
+  prepare_social_share($('html'));
 
   autosize($('[data-action="parti-autoresize"]'));
 });
