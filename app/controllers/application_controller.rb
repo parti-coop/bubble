ed2b3a2ba83b9c49fc4bd38e0d3fd0000cd6dc0b
qus <qus1225@gmail.com>
2016-07-18 09:42:30 +0900

    로그인오류 수정, 관리자이메일 변경

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -62,7 +62,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    redirect_to = request.env["omniauth.params"].try(:fetch, "redirect_to")
+    redirect_to = request.env["omniauth.params"].try(:fetch, "redirect_to", nil)
     redirect_to || stored_location_for(resource) || root_path
   end
 end
