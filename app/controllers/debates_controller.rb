06d92a467518ea2b7e9e2baf492ac4d1d53514d8
qus <qus1225@gmail.com>
2016-07-24 15:30:48 +0900

    쟁점의 같은 입장을 선택했을때 나오는 오류 수정

--- app/controllers/debates_controller.rb
+++ app/controllers/debates_controller.rb
@@ -1,15 +1,17 @@
 class DebatesController < ApplicationController
 
   def choose_alpha
+    render status: 200, json: nil and return if alpha_chosen? params[:slug]
     choose_something('alpha')
   end
 
   def choose_beta
+    render status: 200, json: nil and return if beta_chosen? params[:slug]
     choose_something('beta')
   end
 
   def choose_hold
-    return if hold_chosen? params[:slug]
+    render status: 200, json: nil and return if hold_chosen? params[:slug]
 
     @debate = Debate.find_by slug: params[:slug]
     @debate.alpha_count -= 1 if alpha_chosen? params[:slug]
@@ -26,9 +28,8 @@ class DebatesController < ApplicationController
   def choose_something(choice)
     anti_choice = (choice == 'alpha' ? 'beta' : 'alpha')
 
-    return if chosen? params[:slug], choice
-
     @debate = Debate.find_by slug: params[:slug]
+
     #@debate.alpha_count -= 1 if alpha_chosen? params[:slug]
     @debate.decrement("#{anti_choice}_count") if chosen? params[:slug], anti_choice
     @debate.increment("#{choice}_count")
