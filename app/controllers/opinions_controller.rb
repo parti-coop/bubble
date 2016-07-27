6bd585756c8dd17905c0a38fb8c41b2e9127b696
qus <qus1225@gmail.com>
2016-07-27 17:34:53 +0900

    봇이 remotecall url로 접근하는 경우 나오는 오류를 막습니다.

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -2,6 +2,11 @@ class OpinionsController < ApplicationController
   load_and_authorize_resource
 
   def index
+    respond_to do |format|
+      format.js
+      format.any { redirect_to root_path }
+    end
+
     @debate = Debate.find_by slug: params[:debate_slug]
     @opinions = Opinion.in_debate(@debate).recent.page(params[:page])
   end
