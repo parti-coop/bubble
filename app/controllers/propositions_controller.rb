8c43c078b8b013e8842c625c6f684b340aef914e
muniv <dmtgjh@naver.com>
2016-08-24 18:29:53 +0900

    발의안 댓글을 페이징 합니다 #174

--- app/controllers/propositions_controller.rb
+++ app/controllers/propositions_controller.rb
@@ -1,5 +1,7 @@
 class PropositionsController < ApplicationController
   def show
     @proposition = (Proposition.find_by slug: params["slug"]) || Proposition.most_important
+
+    @proposition_comments=@proposition.comments.recent.page(params[:page]).per(3)
   end
 end
