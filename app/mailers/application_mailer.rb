b30d8e92e32411994df691248bbe29d671346126
dali <dalikim@parti.xyz>
2016-10-21 17:20:27 +0900

    보내는 사람 이메일 주소를 변경합니다

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "시민입법프로젝트 나는알아야겠당 <knowparti@gmail.com>"
+  default from: "시민입법프로젝트 나는알아야겠당 <knowgmo@better-cosmos.net>"
   layout 'mailer'
 end
