5c512f0cbd3484e8b5c699a65d616de6a7d11dea
dali <dalikim@parti.xyz>
2018-02-28 14:34:51 +0900

    사이트 주소를 up.parti.do로 바꿉니다

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,8 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'up.parti.xyz' }
-  config.action_mailer.asset_host =  'http://up.parti.xyz'
+  config.action_mailer.default_url_options = { :host => 'up.parti.do' }
+  config.action_mailer.asset_host =  'http://up.parti.do'
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :postmark
   config.action_mailer.postmark_settings = {
@@ -85,4 +85,4 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 end
 
-Rails.application.routes.default_url_options[:host] = 'up.parti.xyz'
+Rails.application.routes.default_url_options[:host] = 'up.parti.do'
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -63,8 +63,8 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'dev.up.parti.xyz' }
-  config.action_mailer.asset_host =  'http://dev.up.parti.xyz'
+  config.action_mailer.default_url_options = { :host => 'dev.up.parti.do' }
+  config.action_mailer.asset_host =  'http://dev.up.parti.do'
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
@@ -90,4 +90,4 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 end
 
-Rails.application.routes.default_url_options[:host] = 'dev.up.parti.xyz'
+Rails.application.routes.default_url_options[:host] = 'dev.up.parti.do'
